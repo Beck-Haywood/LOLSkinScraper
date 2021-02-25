6d5ceac217d50b47c4bd875624c17460037afb9a
Beck-Haywood <beck.haywood@students.makeschool.com>
2021-02-25 13:16:05 -0900

    I can add every url to GoColly Queue

--- scraper.go
+++ scraper.go
@@ -10,18 +10,26 @@ import (
         // "regexp"
         // "encoding/json"
         // "io/ioutil"
-		"github.com/gocolly/colly"
+        "github.com/gocolly/colly"
+        "github.com/gocolly/colly/queue"
+
 )
 
 func main() {
 		// Instantiate default collector
         c := colly.NewCollector()
+        // create a request queue with 2 consumer threads
+	q, _ := queue.New(
+		2, // Number of consumer threads
+		&queue.InMemoryQueueStorage{MaxSize: 10000}, // Use default queue storage
+	)
         c.OnHTML(".label-only a", func(e *colly.HTMLElement) {
 			link := e.Attr("href")
+                        q.AddURL("https://leagueoflegends.fandom.com" + link + "/Cosmetics")
+                        // fmt.Printf("https://leagueoflegends.fandom.com" + link + "/Cosmetics")
 			e.Request.Visit(link)
 			scrapeSite(link)
         })
-
         c.OnRequest(func(r *colly.Request) {
                 fmt.Println("Visiting", r.URL)
         })
@@ -39,9 +47,8 @@ func main() {
         })
 
         c.Visit("https://leagueoflegends.fandom.com/wiki/List_of_champions")
-
-        // fmt.Printf("%v", names)
-        // fmt.Printf("%v", costs)
+        // fmt.Printf(q)
+        q.Run(c)
 }
 
 func scrapeSite(link string) {
