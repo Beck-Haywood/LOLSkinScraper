e99f77b428632428459149c4a2bad596f177b5fe
Beck-Haywood <beck.haywood@students.makeschool.com>
2021-03-01 18:33:28 -0900

    data returned as map need to add maps together then convert to json

--- scraper.go
+++ scraper.go
@@ -5,8 +5,21 @@ import (
     "fmt"
 	"github.com/gocolly/colly"
 	"strings"
+	"regexp"
 )
+type Skin struct {
+	Name string `json: "name"`
+	Cost string `json: "cost"`
+Date string `json: "date"`
+}
 
+type SkinsStruct struct {
+	Skins []Skin
+}
+
+type ListofSkinsStruct struct {
+	ListofChampionSkins []SkinsStruct
+}
 // scrapes all of the links for each champion
 func linkScrape() {
 	c := colly.NewCollector()
@@ -14,8 +27,7 @@ func linkScrape() {
 	var numOfLinks int
     linkSelector := ".label-only a"
 	links := make(chan string, 180)
-	result := make(chan string, 1)
-
+	result := make(chan  map[string][]Skin, 1)
 
 	c.OnHTML(linkSelector, func(e *colly.HTMLElement) {
         // fmt.Println(e.Text)
@@ -35,8 +47,14 @@ func linkScrape() {
 	close(links)
 
 	for i := 0; i < numOfLinks; i++ {
-		<- result
+		struc := <- result
+		fmt.Println("map:", struc)
 	}
+
+	// Marshal map to json
+
+	// Two choices 1. Add json to json then write to file
+	// create struct and add maps together
 
 	return
 }
@@ -56,13 +74,13 @@ func main() {
 }
 
 // worker gorountine function that scrapes each website concurrently
-func worker(links chan string, result chan string) {
+func worker(links chan string, result chan map[string][]Skin) {
 	for link := range links {
 		result <- champScrape(link)
 	}
 }
 
-func champScrape(link string) string{
+func champScrape2(link string) string{
 	var name string
 	fmt.Println(link)
 	c := colly.NewCollector()
@@ -73,4 +91,71 @@ func champScrape(link string) string{
 	// fmt.Println(name)
 	return name
 
+}
+
+func champScrape(link string) map[string][]Skin {
+	fmt.Println(link)
+	var champMap map[string][]Skin
+
+	var names []string
+	var costs []string
+	var dates []string
+	var name string
+
+	c := colly.NewCollector()
+	c.OnHTML(".mw-redirect", func(e *colly.HTMLElement) {
+		name = e.Text
+	})
+	c.OnHTML(".skin-icon+ div div:nth-child(1)", func(e *colly.HTMLElement) {
+			name := strings.Replace(e.Text, " View in 3D", "", 1)
+			names = append(names, name)
+	})
+	c.OnHTML(".skin-icon+ div div+ div", func(e *colly.HTMLElement) {
+			m1 := regexp.MustCompile(`^[^/]+`)
+			m2 := regexp.MustCompile(`[^/]*$`)
+
+			res := m1.FindString(e.Text)
+			cost := strings.TrimSpace(res)
+
+			res2 := m2.FindString(e.Text)
+			date := strings.TrimSpace(res2)
+
+			costs = append(costs, cost)
+			dates = append(dates, date)
+	})
+	
+
+	c.OnError(func(_ *colly.Response, err error) {
+			fmt.Println("Something went wrong:", err)
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+			fmt.Println("Visited", r.Request.URL)
+	})
+	// c.OnScraped(func(r *colly.Response) {
+	//         fmt.Println("Finished", r.Request.URL)
+	// })
+	c.Visit(link)
+	// fmt.Println(name)
+    // listOfChampSkins := ListofSkinsStruct{} // Init struct for each champ skinStruct
+
+	skins := []Skin{} // Init list skins
+	// skinsStruct := SkinsStruct{skins} // Init struct of skins
+
+
+	for i, name := range names {
+		item1 := Skin{Name: name, Cost: costs[i], Date: dates[i]} // Add all data using index
+		// skinsStruct.AddItem(item1)
+		skins = append(skins, item1)
+		// fmt.Println(name, costs[i], dates[i])
+	}
+	// listOfChampSkins.ListofChampionSkins = append(listOfChampSkins.ListofChampionSkins, skinsStruct) // Append champion skins to listOfChampSkins struct
+	champMap = make(map[string][]Skin)
+	champMap[name] = skins
+	return champMap
+
+}
+func (skins *SkinsStruct) AddItem(skin Skin) []Skin {
+	skins.Skins = append(skins.Skins, skin)
+	return skins.Skins
 }
